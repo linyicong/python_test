a7628f060b73020d6e32239364c1aa2c60f02270
664778717@qq.com <664778717@qq.com>
2019-07-24 16:57:08 +0800

    test jenkins

--- getTianqi.py
+++ getTianqi.py
@@ -2,7 +2,8 @@
 import urllib2
 import json
 from city import city
-cityname=raw_input('你想查那个城市的天气？\n')
+# cityname=raw_input('你想查那个城市的天气？\n')
+cityname = "广州"
 citycode=city.get(cityname)
 
 if citycode:
